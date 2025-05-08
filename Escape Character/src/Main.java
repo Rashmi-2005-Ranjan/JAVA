30b0cf8f06a060d8d001dc3a9db12ef1bba67162
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-08 20:14:43 +0530

    learn About escape character

---
+++ Escape Character/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println ("Hello Kg Coding...");
+
+        //Showing Double Quotes In Output
+        System.out.println ("\"Hello Kg Coding...\"");
+
+        //Showing Single Quotes In Output
+        System.out.println ("\'Hello Kg Coding...\'");
+
+        //Showing Backslash In Output
+        System.out.println ("\\Hello Kg Coding...\\");
+
+        //Showing New Line In Output
+        System.out.println ("Hello Kg Coding 1st Line...\nHello Kg Coding 2nd Line...\nHello Kg Coding 3rd Line...");
+
+        //Showing Tab In Output
+        System.out.println ("H\tello\tKg\tCoding1stLine...");
+
+        //Showing Backspace In Output
+        System.out.println ("Hello Kg Coding 1st Line...\bHello Kg Coding 2nd Line...\bHello Kg Coding 3rd Line...");
+    }
+}
