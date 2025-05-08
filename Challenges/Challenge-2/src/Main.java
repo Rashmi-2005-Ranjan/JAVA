7e087073f82ec10a995ac55e4f770b4b1ffe34cd
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-08 20:10:50 +0530

    Practiced Escape Sequence Characters With Printing Patterens Manualli

---
+++ Challenges/Challenge-2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print a right-angled triangle of stars with concept of escape sequences
+        System.out.println ("*\n* *\n* * *\n* * * *\n* * * * *\n");
+    }
+}
---
+++ Challenges/Challenge-2/src/ReverseRightHalfPyramid.java
@@ -0,0 +1,6 @@
+public class ReverseRightHalfPyramid {
+    public static void main(String[] args) {
+        // Printing right Half reverse Pyramid
+        System.out.println ("* * * * *\n* * * *\n* * *\n* *\n*" );
+    }
+}
