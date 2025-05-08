76eb865fc24fb9afa2c21f7046672e6e3a9527ec
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-08 19:03:29 +0530

    Variables Datatypes

---
+++ Data Types , Variable , Input/variables.java
@@ -0,0 +1,32 @@
+public class variables {
+    public static void main(String[] args) {
+        // Integer Variables
+        int myNumber = 865;
+
+        int yourNumber;
+
+        yourNumber = 457;
+
+        System.out.println("myNumber: " + myNumber);
+        System.out.println("yourNumber: " + yourNumber);
+
+        //Boolean Variables
+        boolean isVegetarian= false;
+        System.out.println("Is Vegetarian: " + isVegetarian);
+
+        float myFloat = 5.0f;
+        System.out.println ("myFloat: " + myFloat);
+
+        //Double Variables
+        double myDouble = 5.45786;
+        System.out.println("myDouble: " + myDouble);
+
+        //String Variables
+        String myString = "Good Morning";
+        System.out.println("myString: " + myString);
+
+        //Character Variables
+        char myCharacter='a';
+        System.out.println("myCharacter: " + myCharacter);
+    }
+}
