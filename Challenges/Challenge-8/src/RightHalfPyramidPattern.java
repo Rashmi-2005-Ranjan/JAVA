4b35003baa4bdba3af24a594c728dec0d13dd737
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-10 23:44:02 +0530

    Pattern Of right half Pyramid Using While loop

---
+++ Challenges/Challenge-8/src/RightHalfPyramidPattern.java
@@ -0,0 +1,15 @@
+public class RightHalfPyramidPattern {
+    public static void main(String[] args) {
+        int rows=0;
+        while (rows<5){
+            System.out.print ("*" );
+            int i=0;
+            while(i<rows){
+                System.out.print (" *" );
+                i++;
+            }
+            System.out.println ();
+            rows++;
+        }
+    }
+}
