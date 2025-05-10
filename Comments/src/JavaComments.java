b62e07a7972e731c1b3e8a89a3447b501d0c0189
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-10 22:10:28 +0530

    Java Comments

---
+++ Comments/src/JavaComments.java
@@ -0,0 +1,24 @@
+public class JavaComments {
+    /**
+     *
+     *This is Main Method docs
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println ("Java Comments");
+
+        // This is a single-line comment
+
+
+        /*
+         * This is a multi-line comment
+         * that spans multiple lines
+         */
+
+
+        /**
+         * This is a documentation comment
+         * that can be used to generate documentation
+         */
+    }
+}
