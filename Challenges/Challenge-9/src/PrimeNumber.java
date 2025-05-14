56a744f1bf4fe899425a2bcd4095e3892b31a991
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-14 23:17:40 +0530

    Prime Number Code Optimized

--- Challenges/Challenge-9/src/PrimeNumber.java
+++ Challenges/Challenge-9/src/PrimeNumber.java
@@ -32,7 +32,7 @@ public class PrimeNumber {
             return true;
         }
 
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=Math.sqrt ( n );i++){
             if(n%i==0){
                 return false;
             }
