d4f1fa8ad81f0ac0f86e596674a4896310f375e3
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-23 23:46:44 +0530

    Understanding Of this Keyword

--- Object Oriented Programming/src/Car.java
+++ Object Oriented Programming/src/Car.java
@@ -7,23 +7,28 @@ public class Car {
     int noOfSeats;
 
     //Methods ====> Behaviors ====>Instance Methods
-    public void drive() {
+    public Car start(){
         if (currentFuelInLiters <= 0) {
-            System.out.println("Car is out of fuel");
+            System.out.println("Car is out of fuel cannot start");
         } else if (currentFuelInLiters<5) {
             System.out.println ("Car is low on fuel please refuel");
             currentFuelInLiters--;
         } else{
-            System.out.println("Car is driving");
+            System.out.println("Car is started...chihihinhing");
             currentFuelInLiters--;
         }
+        return this;
+    }
+    public void drive() {
+            currentFuelInLiters--;
+            System.out.println("Car is driving");
     }
 
-    public void addFuel(float fuel){
-        currentFuelInLiters+= fuel;
+    public void addFuel(float currentFuelInLiters){
+        this.currentFuelInLiters+= currentFuelInLiters;
     }
 
-    public float getCurrentFuelLeft(){
+    public float getCurrentFuelLeft() {
         return currentFuelInLiters;
     }
 }
--- Object Oriented Programming/src/Driver.java
+++ Object Oriented Programming/src/Driver.java
@@ -1,15 +1,20 @@
 public class Driver {
     public static void main(String[] args) {
-        Car myCar=new Car ();
-        myCar.addFuel (6);
-        myCar.drive ();
-        System.out.println (myCar.getCurrentFuelLeft ());
-        myCar.drive ();
-        System.out.println (myCar.getCurrentFuelLeft ());
-        myCar.drive ();
-        System.out.println (myCar.getCurrentFuelLeft ());
-        myCar.addFuel ( 5);
-        myCar.drive ();
-        System.out.println (myCar.getCurrentFuelLeft ());
+//        Car myCar=new Car ();
+//        myCar.addFuel (6);
+//        myCar.drive ();
+//        System.out.println (myCar.getCurrentFuelLeft ());
+//        myCar.drive ();
+//        System.out.println (myCar.getCurrentFuelLeft ());
+//        myCar.drive ();
+//        System.out.println (myCar.getCurrentFuelLeft ());
+//        myCar.addFuel ( 5);
+//        myCar.drive ();
+//        System.out.println (myCar.getCurrentFuelLeft ());
+
+        Car swift=new Car ();
+        swift.addFuel ( 6 );
+        Car startedCar=swift.start ();
+        startedCar.drive ();
     }
 }
