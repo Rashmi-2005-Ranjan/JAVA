e55cce4e4e8911f9151ef1293129aef844b00759
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-24 21:50:53 +0530

    This , Static Variable , Keyword , Block

--- Object Oriented Programming/src/Car.java
+++ Object Oriented Programming/src/Car.java
@@ -1,34 +1,68 @@
 public class Car {
-    //Properties ====> Instance Variables
+    static int noOfCarsSold;
     int noOfWheels;
     String color;
     float maxSpeed;
     float currentFuelInLiters;
     int noOfSeats;
 
-    //Methods ====> Behaviors ====>Instance Methods
-    public Car start(){
-        if (currentFuelInLiters <= 0) {
-            System.out.println("Car is out of fuel cannot start");
-        } else if (currentFuelInLiters<5) {
-            System.out.println ("Car is low on fuel please refuel");
-            currentFuelInLiters--;
-        } else{
-            System.out.println("Car is started...chihihinhing");
-            currentFuelInLiters--;
+    static {
+        noOfCarsSold = 0;
+        System.out.println("I am in Static Block");
+    }
+
+    {
+        noOfCarsSold++;
+        System.out.println("I am in Init Block");
+    }
+
+    Car(String color) {
+        noOfWheels = 4;
+        this.color = color;
+        maxSpeed = 150;
+        currentFuelInLiters = 2;
+        noOfSeats = 5;
+    }
+
+    Car() {
+        this("Black");
+        currentFuelInLiters = 5;
+    }
+
+    public Car start() {
+        int pop = 5;
+        if (currentFuelInLiters == 0) {
+            System.out.println(pop);
+            System.out.println("Car is out if fuel, can not start");
+        } else if (currentFuelInLiters < 5) {
+            System.out.println("Car is in reserved mode, please refuel");
+        } else {
+            System.out.println("Car is started.. bruhhhh.....");
         }
         return this;
     }
+
     public void drive() {
-            currentFuelInLiters--;
-            System.out.println("Car is driving");
+        currentFuelInLiters--;
+        System.out.println("Car is driving");
     }
 
-    public void addFuel(float currentFuelInLiters){
-        this.currentFuelInLiters+= currentFuelInLiters;
+    public void addFuel(float currentFuelInLiters) {
+        this.currentFuelInLiters += currentFuelInLiters;
     }
 
-    public float getCurrentFuelLeft() {
+    public float getCurrentFuelLevel() {
         return currentFuelInLiters;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "noOfWheels=" + noOfWheels +
+                ", color='" + color + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", currentFuelInLiters=" + currentFuelInLiters +
+                ", noOfSeats=" + noOfSeats +
+                '}';
     }
 }
