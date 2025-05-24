74789611fb70505d7674d38eff42e969ce8e2297
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-24 21:52:31 +0530

    Practice Concepts

--- Object Oriented Programming/src/Driver.java
+++ Object Oriented Programming/src/Driver.java
@@ -1,20 +1,35 @@
 public class Driver {
-    public static void main(String[] args) {
-//        Car myCar=new Car ();
-//        myCar.addFuel (6);
-//        myCar.drive ();
-//        System.out.println (myCar.getCurrentFuelLeft ());
-//        myCar.drive ();
-//        System.out.println (myCar.getCurrentFuelLeft ());
-//        myCar.drive ();
-//        System.out.println (myCar.getCurrentFuelLeft ());
-//        myCar.addFuel ( 5);
-//        myCar.drive ();
-//        System.out.println (myCar.getCurrentFuelLeft ());
 
-        Car swift=new Car ();
-        swift.addFuel ( 6 );
-        Car startedCar=swift.start ();
-        startedCar.drive ();
+    static int minAgeForDriving = 18;
+
+    String name;
+    int age;
+    String dateOfLicense;
+
+    public boolean isAllowedToDrive() {
+        return this.age >= minAgeForDriving;
+    }
+
+
+    public static void main(String[] args) {
+//        Car myCar = new Car();
+//        myCar.addFuel(6);
+//        myCar.drive();
+//        myCar.drive();
+//        myCar.drive();
+//        myCar.addFuel(3);
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+
+        Car swift = new Car("Red");
+        Car thar = new Car();
+        thar = null;
+        //swift.addFuel(6);
+        swift.start().drive();
+        System.out.println(swift.color);
+
+//        Driver myDriver = new Driver();
+//        myDriver.dateOfLicense = "1/Jan/2024";
+//        System.out.println(minAgeForDriving);
     }
 }
