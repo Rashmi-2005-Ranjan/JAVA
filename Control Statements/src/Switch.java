d414779bf7b2dcd7192abeacaff2d823b5212f3d
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-05-25 20:21:42 +0530

    Switch Statement in Java

--- Control Statements/src/Switch.java
+++ Control Statements/src/Switch.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Switch {
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Day of the Week Checker");
+        System.out.print("Enter a number (1-7) for the day of the week: ");
+
+        int day = input.nextInt (); // Example day of the week (1-7)
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+        }
+
+        System.out.println("The day is: " + dayName);
+    }
 }
